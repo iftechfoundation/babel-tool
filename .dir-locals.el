51de2e2c5bf743cd22b71b39785a8c56f1c4972e
Thomas Nilefalk <thomas@nilefalk.se>
2020-10-02 23:12:37 +0200

    Add .dir-locals for Emacs c-mode

---
+++ .dir-locals.el
@@ -0,0 +1,12 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+(
+ (c-mode . (
+            (indent-tabs-mode . nil)
+            (c-file-style . "gnu")
+            (c-basic-offset . 2)
+            (eval . (c-set-offset 'arglist-close '0))
+            )
+         )
+ )
