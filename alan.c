2d35f2fbe861aa5e2f8198a179030d12817a4faf
Thomas Nilefalk <thomas@nilefalk.se>
2020-10-03 09:55:51 +0200

    Find IFID in Alan v3 files

--- alan.c
+++ alan.c
@@ -21,35 +21,60 @@
 
 typedef unsigned char byte;
 
-static int32 read_alan_int_at(unsigned char *from)
+static int32 read_alan_int_at(byte *from)
 {
   return ((unsigned long int) from[3])| ((unsigned long int)from[2] << 8) |
     ((unsigned long int) from[1]<<16)| ((unsigned long int)from[0] << 24);
+}
+
+static bool magic_word_found(byte *story_file) {
+  return memcmp(story_file,"ALAN",4) == 0;
 }
 
 static int32 get_story_file_IFID(void *story_file, int32 extent, char *output, int32 output_extent)
 {
   if (story_file || extent)
     { }
-  ASSERT_OUTPUT_SIZE(6);
-  strcpy(output,"ALAN-");
-  return INCOMPLETE_REPLY_RV;
+
+  if (!magic_word_found(story_file))
+    {
+      ASSERT_OUTPUT_SIZE(5);
+      strcpy(output,"ALAN-");
+      return INCOMPLETE_REPLY_RV;
+    }
+  else
+    {
+      /*
+        Alan v3 stores IFIDs in the story file in the following format:
+
+        ACodeHeader[48] - Aword address to the IFID table
+        IFID-table: any number of tuples containing
+        - Aword address to name of IFID
+        - Aword address to value
+        The table is terminated with -1
+
+        The IFID named IFID is always the first.
+      */
+      byte *memory = (byte *)story_file;
+
+      ASSERT_OUTPUT_SIZE(45);
+      int32 ifid_table_address = read_alan_int_at((byte *)&memory[48*4]);
+      int32 ifid_value_address = read_alan_int_at((byte *)&memory[(ifid_table_address+1)*4]);
+      strncpy(output, (char *)&memory[ifid_value_address*4], 45);
+
+      return VALID_STORY_FILE_RV;
+    }
 }
 
 
-static bool magic_word_found(byte *sf) {
-  return memcmp(sf,"ALAN",4) == 0;
-}
-
-
-static bool correct_crc(byte *sf, int32 size_in_awords) {
+static bool crc_is_correct(byte *story_file, int32 size_in_awords) {
   /* Size of AcodeHeader is 50 Awords = 200 bytes */
   int32 crc = 0;
 
   for (int i=50*4;i<(size_in_awords*4);i++)
-    crc+=sf[i];
+    crc+=story_file[i];
 
-  return (crc == read_alan_int_at(sf+46*4));
+  return (crc == read_alan_int_at(story_file+46*4));
 }
 
 
@@ -72,11 +97,12 @@ static int32 claim_story_file(void *story_file, int32 extent_in_bytes)
     /* File is shorter than any Alan file header */
     return INVALID_STORY_FILE_RV;
 
+  size_in_awords=read_alan_int_at(sf+4);
+
   if (!magic_word_found(sf))
     { /* Identify Alan 2.x */
       int32 crc = 0;
 
-      size_in_awords=read_alan_int_at(sf+4);
       if (size_in_awords > extent_in_bytes/4) return INVALID_STORY_FILE_RV;
       for (i=24;i<81;i+=4)
         if (read_alan_int_at(sf+i) > extent_in_bytes/4) return INVALID_STORY_FILE_RV;
@@ -89,7 +115,7 @@ static int32 claim_story_file(void *story_file, int32 extent_in_bytes)
     { /* Identify Alan 3 */
       size_in_awords=read_alan_int_at(sf+3*4); /* hdr.size @ 3 */
 
-      if (!correct_crc(sf, size_in_awords))
+      if (!crc_is_correct(sf, size_in_awords))
         return INVALID_STORY_FILE_RV;
 
       return VALID_STORY_FILE_RV;
