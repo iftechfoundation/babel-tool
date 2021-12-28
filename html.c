dbd5bef332197a9b4a0fe43b8dbde66aa8036c91
Dan Fabulich <dan.fabulich@redfin.com>
2021-12-28 13:31:28 -0800

    Add support for IFIDs in non-Twine HTML

--- html.c
+++ html.c
@@ -40,37 +40,44 @@ static int32 find_text_in_file(void *story_file, int32 extent, char *str)
     return -1;
 }
 
-static int32 get_story_file_IFID(void *story_file, int32 extent, char *output, int32 output_extent)
-{
-    int32 pos = find_text_in_file(story_file, extent, "<tw-storydata");
-    if (pos != -1) {
-        /* Search the <tw-storydata> tag for ifid="..." */
-        void *starttag = story_file + pos;
-        void *endtag = memchr(starttag, '>', extent-pos);
-        if (endtag) {
-            int32 attrpos = find_text_in_file(starttag, endtag-starttag, "ifid=\"");
-            if (attrpos != -1) {
-                attrpos += 6; /* skip the ifid=" */
-                void *endattr = memchr(starttag+attrpos, '"', endtag-(starttag+attrpos));
-                if (endattr) {
-                    /* Got it. */
-                    int32 attrlen = endattr - (starttag+attrpos);
-                    ASSERT_OUTPUT_SIZE(attrlen+1);
-                    memcpy(output, starttag+attrpos, attrlen);
-                    output[attrlen] = '\0';
-                    return VALID_STORY_FILE_RV;
-                }
+static int32 find_attribute_value(void *story_file, int32 extent, char *output, int32 output_extent, int32 pos, char* attribute_prefix) {
+    void *starttag = story_file + pos;
+    void *endtag = memchr(starttag, '>', extent-pos);
+    if (endtag) {
+        int32 attrpos = find_text_in_file(starttag, endtag-starttag, attribute_prefix);
+        if (attrpos != -1) {
+            attrpos += strlen(attribute_prefix);
+            void *endattr = memchr(starttag+attrpos, '"', endtag-(starttag+attrpos));
+            if (endattr) {
+                /* Got it. */
+                int32 attrlen = endattr - (starttag+attrpos);
+                ASSERT_OUTPUT_SIZE(attrlen+1);
+                memcpy(output, starttag+attrpos, attrlen);
+                output[attrlen] = '\0';
+                return VALID_STORY_FILE_RV;
             }
         }
-
-        /* Couldn't find the ifid attribute. */
-        return INVALID_STORY_FILE_RV;
     }
-    
-    /* This version of Twine is too old to include an IFID, so we'll
-       generate one from the MD5. */
+    /* Couldn't find the attribute. */
+    return INVALID_STORY_FILE_RV;
+}
+
+static int32 get_story_file_IFID(void *story_file, int32 extent, char *output, int32 output_extent)
+{
+    int32 pos = find_text_in_file(story_file, extent, "<meta property=\"ifiction:ifid\"");
+    if (pos != -1) {
+        return find_attribute_value(story_file, extent, output, output_extent, pos, "content=\"");
+    }
+
+    /* Twine 2 */
+    pos = find_text_in_file(story_file, extent, "<tw-storydata");
+    if (pos != -1) {
+        return find_attribute_value(story_file, extent, output, output_extent, pos, "ifid=\"");
+    }
+
+    /* Generate IFID from MD5 */
     ASSERT_OUTPUT_SIZE(8);
-    strcpy(output,"TWINE-");
+    strcpy(output,"HTML-");
     return INCOMPLETE_REPLY_RV;
 }
 
@@ -85,6 +92,16 @@ static int32 claim_story_file(void *story_file, int32 extent)
         /* Twine 1, almost certainly. */
         return VALID_STORY_FILE_RV;
     }
-        
+
+    /* every string counts as invalid HTML, but these are pretty definitive */
+    if (find_text_in_file(story_file, extent, "<html") != -1) {
+        return VALID_STORY_FILE_RV;
+    }
+    if (find_text_in_file(story_file, extent, "<!doctype html") != -1) {
+        return VALID_STORY_FILE_RV;
+    }
+    if (find_text_in_file(story_file, extent, "<meta property=\"ifiction:ifid\"") != -1) {
+        return VALID_STORY_FILE_RV;
+    }
     return INVALID_STORY_FILE_RV;
 }
