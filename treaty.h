caea7673f0db137c1e68216fc4251115f7ae9534
Andrew Plotkin <zarf@ZarfLent.local>
2026-09-18 21:22:32 -0400

    Treaty version number.

--- treaty.h
+++ treaty.h
@@ -13,8 +13,8 @@
 
 #define TREATY_H
 
-#define TREATY_COMPLIANCE "Treaty of Babel revision 12"
-#define TREATY_VERSION "r12"
+#define TREATY_COMPLIANCE "Treaty of Babel revision 13"
+#define TREATY_VERSION "r13"
 
 /* return codes */
 #define NO_REPLY_RV                    0
